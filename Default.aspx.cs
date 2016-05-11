aa89848d2db0d52cdbf132619ed11cd4fcd76c3e
Chuck D'Antonio <c_dantonio@acm.org>
2016-05-11 12:36:26 -0600

    Initial cut during workshop

---
+++ Default.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Collections.Generic;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      HelloWorldLabel.Text = "Hello from .NET";
+    }
+}
